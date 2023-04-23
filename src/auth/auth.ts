0343ebd26673dcda723f156a514ad22527ba4d78
HipsterPenguin <wyss.craiga@gmail.com>
2023-04-23 18:21:16 -0500

    fixed wrong naming for environment variable

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -19,8 +19,8 @@ export const authenticateToken =  async (req: Request, res: Response, next: Next
 const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if (token == null) return res.sendStatus(401);
-  if (process.env.ACCESS_TOKEN_SECRET) {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  if (process.env.JWT_SECRET) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) return res.sendStatus(403);
       req.user = user as AuthUser;
       next();
