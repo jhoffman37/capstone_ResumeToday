8ef091de05582ba347a3db19bec4ecf523c8221d
pirate4593 <joconnor10@student.cvtc.edu>
2023-04-27 10:23:44 -0500

    [Finishes #184963881] Added Light/Dark toggle button

---
+++ public/toggle.js
@@ -0,0 +1,33 @@
+function toggleMode() {
+  var cssMode = document.querySelector('#default');
+  var logo = document.querySelector('#logo');
+  var currentMode = cssMode.getAttribute('href');
+  var newMode;
+
+  if (currentMode == '../site.css') {
+    newMode = '../site_dark.css';
+    logo.src = "../ResumeTodayLogo_dark.png";
+  } else {
+    newMode = '../site.css';
+    logo.src = "../ResumeTodayLogo.png";
+  }
+
+  cssMode.setAttribute('href', newMode);
+  localStorage.setItem('preferredMode', newMode);
+}
+
+var toggleBtn = document.getElementById('toggle-btn');
+toggleBtn.addEventListener('click', toggleMode);
+
+// check if user has a preferred mode and set it
+var preferredMode = localStorage.getItem('preferredMode');
+if (preferredMode) {
+  var cssMode = document.querySelector('#default');
+  var logo = document.querySelector('#logo');
+  cssMode.setAttribute('href', preferredMode);
+  if (preferredMode == '../site_dark.css') {
+    logo.src = "../ResumeTodayLogo_dark.png";
+  } else {
+    logo.src = "../ResumeTodayLogo.png";
+  }
+}
