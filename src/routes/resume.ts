e8c227dcf40f8729769f735423cb1a9745f8e8c1
lucidus115 <minecrafter141542@gmail.com>
2023-05-03 12:05:07 -0500

    Verify user owns the resume before viewing

--- src/routes/resume.ts
+++ src/routes/resume.ts
@@ -16,7 +16,7 @@ type FormData = {
 }
 
 router.get("/resume-edit", authenticateToken, (req: Request, res: Response) => {
-  res.render("pages/edit.ejs", {user: req.user ? req.user : null});
+  res.render("pages/edit.ejs", {user: req.user ? req.user : null})
 })
 router.get("/resume-edit/:id", authenticateToken, async (req: Request, res: Response) => {
   const id = Number.parseInt(req.params.id);
@@ -24,6 +24,14 @@ router.get("/resume-edit/:id", authenticateToken, async (req: Request, res: Resp
 
   if (!resume_data) {
     const msg = `Could not load resume as it does not exist`;
+    res.render("pages/edit.ejs", { user: req.user ? req.user : null,
+      msg
+    });
+    return;
+  }
+
+  if (!req.user || resume_data.user_id !== req.user.id) {
+    const msg = `You do not have permission to edit this resume`;
     res.render("pages/edit.ejs", { user: req.user ? req.user : null,
       msg
     });
@@ -86,6 +94,11 @@ router.get("/resume-view/:id", authenticateToken, async (req: Request, res: Resp
   try {
     const id: number = Number.parseInt(req.params.id);
     const resume: Resume = await ResumeDB.get(id);
+
+    if (!req.user || resume.user_id !== req.user.id) {
+      res.status(401).send('You do not have permission to view this resume')
+      return;
+    }
 
     res.render("pages/resume.ejs", { user: req.user ? req.user : null,
       title: resume.title,
