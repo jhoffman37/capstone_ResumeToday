23e75e8269c17b0812714bfae8ef5beeb16fd8fb
pirate4593 <joconnor10@student.cvtc.edu>
2023-05-18 17:29:40 -0500

    Flash fix

--- public/toggle.js
+++ public/toggle.js
@@ -4,15 +4,22 @@ function toggleMode() {
 
   body.classList.toggle('dark-mode');
   logo.src = body.classList.contains('dark-mode') ? "../ResumeTodayLogo_dark.png" : "../ResumeTodayLogo.png";
-  
-  var currentMode = body.classList.contains('dark-mode') ? '../site_dark.css' : '../site.css';
+
+  // Toggle background and text colors
+  var bgColor = body.classList.contains('dark-mode') ? '#000' : '#fff';
+  var textColor = body.classList.contains('dark-mode') ? '#fff' : '#000';
+  body.style.backgroundColor = bgColor;
+  body.style.color = textColor;
+
+  // Update preferred mode in localStorage
+  var currentMode = body.classList.contains('dark-mode') ? 'dark' : 'light';
   localStorage.setItem('preferredMode', currentMode);
 
   // Gradual transition to dark mode
   body.classList.add('transition');
   window.setTimeout(function() {
     body.classList.remove('transition');
-  }, 1);
+  }, 1000);
 }
 
 var toggleBtn = document.getElementById('toggle-btn');
